c49ec9d7bf8ad2b7bed5aacf556e58df9cd4eccd
Luis Zhang <luis.zhang@Live.com.au>
2024-03-22 12:19:07 +1100

    Moved minecraft folder to external folder

--- startmine.sh
+++ startmine.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 process_name="server"
+minecraft_path="C:\Users\luisz\OneDrive\Desktop\Minecraft"
 
 if pgrep -x "$process_name" >/dev/null; then
-echo "The process $process_name is already running."
-exit 0
+    echo "The process $process_name is already running."
+    exit 0
 else 
-echo "Starting $process_name..." 
-cd Minecraft || { echo "Error: Minecraft folder not found."; exit 1; }
-./start_server.sh
-echo Server Starting command has been executed
+    echo "Starting $process_name..." 
+    cd "$minecraft_path" || { echo "Error: Minecraft folder not found."; exit 1; }
+    ./start_server.sh
+    echo Server Starting command has been executed
 fi
 
