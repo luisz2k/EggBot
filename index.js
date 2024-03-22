34947cefd6b6ae56ab172dfd4b7a4816a405521d
Luis Zhang <luis.zhang@Live.com.au>
2024-03-22 13:06:24 +1100

    Implement egg status command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const { handleTerminalCommand } = require("./terminalCommand");
+const { exec } = require("child_process");
 
 const client = new Client({
   intents: [
@@ -26,6 +27,28 @@ client.on("messageCreate", (message) => {
 
   if (message.content === "Hi") {
     message.reply("Hello!");
+  }
+
+  if (message.content === "mmhmm") {
+    message.reply("MMHMM MMHMM");
+  }
+
+  if (message.content === "!egg status") {
+    // Use tasklist command to list all running processes
+    exec("tasklist", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing tasklist: ${error}`);
+        message.reply("Error checking Minecraft server status");
+        return;
+      }
+
+      // Check if java.exe is present in the tasklist output
+      if (stdout.includes("java.exe")) {
+        message.reply("Minecraft server is up and running");
+      } else {
+        message.reply("Minecraft server is off");
+      }
+    });
   }
 });
 
