33294b7eb0762f79eadf36dd30534851807c1486
Luis Zhang <luis.zhang@Live.com.au>
2025-07-07 17:51:49 +1000

    Updated bat file to launch new fabric server

--- startmine.bat
+++ startmine.bat
@@ -1,17 +1,17 @@
 @echo off
 set process_name=server
-set minecraft_path=C:\Users\luisz\OneDrive\Desktop\Minecraft
+set minecraft_path=C:\Users\luisz\OneDrive\Desktop\Fabric_Server
 
 tasklist /FI "IMAGENAME eq %process_name%.exe" 2>NUL | find /I /N "%process_name%">NUL
-if "%ERRORLEVEL%"=="0" (
+if "%ERRORLEVEL%" == "0" (
     echo The process %process_name% is already running.
     exit /B 0
 ) else (
     echo Starting %process_name%...
     cd /D "%minecraft_path%" || (
-        echo Error: Minecraft folder not found.
+        echo Error: Fabric server folder not found.
         exit /B 1
     )
-    call start_server.bat
+    call run.bat
     echo Server Starting command has been executed
 )
